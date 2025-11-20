97154d5b30bea26ec7e5cef8e32e587d5b988847
Mika Braginsky <mika.br@gmail.com>
2025-11-19 16:35:35 -0800

    add process dataset script, add single task upload to registry

--- scripts/scoring/upload_registry.R
+++ scripts/scoring/upload_registry.R
@@ -1,11 +1,20 @@
 library(redivis)
-library(tidyverse)
+library(purrr)
+library(stringr)
+library(glue)
 
-registry_table <- redivis$organization("levante")$dataset("scoring:e97h")$table("model_registry")
+registry_table <- redivis$organization("levante")$dataset("levante-metadata-scoring:e97h", version = "next")$table("model_registry:rqwv")
 
 regdir <- "02_scoring_outputs/model_registry"
 regfiles <- list(name = list.files(regdir, recursive = TRUE),
                  path = list.files(regdir, recursive = TRUE, full.names = TRUE)) |>
   transpose()
 
-registry_table$add_files(files = regfiles)
+upload_registry <- \(regfiles) {
+  registry_table$add_files(files = regfiles)
+}
+
+upload_registry_task <- \(regfiles, task) {
+  task_files <- regfiles |> keep(\(rf) str_detect(rf$name, glue("^{task}/")))
+  registry_table$add_files(files = task_files)
+}
